a77b095f350f6e9deed91ae8b00f06b6f086b455
lazypanda31 <83162435+lazypanda31@users.noreply.github.com>
2022-10-05 16:18:53 +0530

    - navBar active link - create '/about' route

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -2,18 +2,20 @@ import React,{useContext} from 'react'
 import ThemeContext from '../../store/themeContext';
 import styles from '../../styles/NavBar.module.css'
 import Link from "next/link";
-import {useState} from 'react'
+import { useRouter } from "next/router";
 
 
 function NavBar() {
-    const themeCtx = useContext(ThemeContext)
-    
+  const themeCtx = useContext(ThemeContext)
+  const router = useRouter();
 
   return (
     <div className={``}>
       <div className="absolute w-3/4 m-auto left-0 right-0">
         <div
-          className={`flex justify-between content-center pt-8 pb-8  ${styles.navFont}`}
+          className={`flex justify-between content-center pt-8 pb-8  ${
+            styles.navFont
+          }  ${themeCtx.isDarkMode ? "text-white" : ""}`}
         >
           <div
             className={`font-bold text-3xl cursor-pointer ${
@@ -25,21 +27,27 @@ function NavBar() {
           </div>
           <div className={`flex content-center ${styles.navFont}`}>
             <div
-              className={`text-[#E63946] px-4 py-1 cursor-pointer font-bold`}
+              className={` ${
+                router.pathname === "/" ? "text-[#E63946]" : "text-white"
+              } px-4 py-1 cursor-pointer font-bold`}
             >
               <Link href="/">Home</Link>
             </div>
             <div
               className={`px-4 py-1 cursor-pointer font-bold ${
                 themeCtx.isDarkMode ? "text-white" : ""
-              }`}
+              }
+              ${router.pathname === "/about" ? "text-[#E63946]" : "text-white"}
+              `}
             >
               <Link href="">About me</Link>
             </div>
             <div
-              className={`px-4 py-1 cursor-pointer font-bold  ${
-                themeCtx.isDarkMode ? "text-white" : ""
-              }`}
+              className={`px-4 py-1 cursor-pointer font-bold 
+              ${
+                router.pathname === "/contact" ? "text-[#E63946]" : "text-white"
+              }
+              `}
             >
               <Link href="/contact">Contact</Link>
             </div>
